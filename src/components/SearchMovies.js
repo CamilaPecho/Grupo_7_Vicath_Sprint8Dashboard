c9f446bfc037646ea8e5e731e482c73185cf780f
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2022-04-04 19:43:10 -0300

    Limberth - correccion imagen buscar prod

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -70,7 +70,7 @@ function SearchMovies(){
 												<div className="text-center">
 													<img 
 														className="img-fluid px-3 px-sm-4 mt-3 mb-4" 
-														src={product.imagenPrincipal}
+														src={'https://vicath-libreria.herokuapp.com'+product.imagenPrincipal}
 														alt={product.name} 
 														style={{ width: '90%', height: '400px', objectFit: 'cover' }} 
 													/>
